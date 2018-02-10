e78ac3f129de8f4d05a765b522b24372f8e2ca70
Vito Mastromarino <zlatanito88@gmail.com>
2018-02-10 16:55:27 +0100

    Add test for route DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -84,8 +84,10 @@ describe('GET /todos/:id', () => {
 
     it('should get a todo by its id', (done) => {
 
+        var hexId = todos[0]._id.toHexString();
+
         request(app)
-        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .get(`/todos/${hexId}`)
         .expect(200)
         .expect((res)=> {
             expect(res.body.todo.text).toBe(todos[0].text);
@@ -96,8 +98,10 @@ describe('GET /todos/:id', () => {
 
     it('should return 404 if todo not found', (done) => {
 
+        var hexId = new ObjectID().toHexString();
+
         request(app)
-        .get(`/todos/${new ObjectID().toHexString()}`)
+        .get(`/todos/${hexId}`)
         .expect(404)
         .end(done);
 
@@ -107,6 +111,51 @@ describe('GET /todos/:id', () => {
 
         request(app)
         .get('/todos/123')
+        .expect(404)
+        .end(done);
+        
+    });
+
+});
+
+describe('DELETE /todos/:id', () => {
+
+    it('should delete a todo by its id', (done) => {
+
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist;
+                done();
+            }).catch((e) => done(e));
+        });
+
+    });
+
+    it('should return 404 if todo not found', (done) => {
+
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+
+        request(app)
+        .delete('/todos/123')
         .expect(404)
         .end(done);
         
