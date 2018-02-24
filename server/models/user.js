3cf5b9312b09b31ad236cd42da6416e386a1b6ae
Vito Mastromarino <zlatanito88@gmail.com>
2018-02-24 23:53:42 +0100

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -45,7 +45,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access}, '123abc').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens.push({ access, token });
 
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function(token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, '123abc');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } 
     catch(e) {
         // return new Promise((resolve, reject) => {
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
     password: 'userOnePass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userOneId.toHexString(), access:'auth'}, '123abc').toString()
+        token: jwt.sign({_id: userOneId.toHexString(), access:'auth'}, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id: userTwoId,
@@ -20,7 +20,7 @@ const users = [{
     password: 'userTwoPass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id: userTwoId.toHexString(), access:'auth'}, '123abc').toString()
+        token: jwt.sign({_id: userTwoId.toHexString(), access:'auth'}, process.env.JWT_SECRET).toString()
     }]
 }];
 
